05c39da8f3fd7a6ca59d642e6609a4c0ea4bbcea
PfkL <pfklsdt@hotmail.com>
2024-04-28 13:32:25 -0400

    ville

--- Program.cs
+++ Program.cs
@@ -8,19 +8,12 @@ namespace Condition
         static void Main(string[] args)
         {
             // Déclaration des variables utilisées par l’application.
-            int temperature = 0;
+            string ville = null;
 
             Console.WriteLine();
-            // Récupération de la température saisie par l’utilisateur
-            Console.Write("Donnez-moi la température actuelle en degré Fahrenheit : ");
-            temperature = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
-
-            // Utilisation de l'opérateur ternaire pour afficher un message en fonction de la température
-            Console.WriteLine(temperature <= 32 ? "Brrr ! Il fait froid aujourd’hui." :
-                              temperature <= 60 ? "C’est mieux que d’être sous le point de congélation." :
-                              temperature <= 90 ? "La température est bonne." :
-                              "Il commence à faire trop chaud !");
+            // Affichage du nom de la ville si la variable a été initialisée
+            string nomVille = ville ?? "Le nom de la ville est inconnu."; // Utilisation de guillemets droits
+            Console.WriteLine(" " + nomVille); // Utilisation de guillemets droits
             Console.ReadKey();
         }
     }
